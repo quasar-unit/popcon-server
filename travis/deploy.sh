3dfac1d8ad53ba2a571e3ed95a4de808bab6c104
JCooky <bak723@gmail.com>
2015-01-26 00:55:02 +0900

    update. travis.yml and add deploy shell script

---
+++ travis/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+MY_USER=$1
+MY_PASSWORD=$2
+SSH_OPTS="-o StrictHostKeyChecking=no"
+
+SSHPASS="sshpass -p ${MY_PASSWORD}"
+SCP="${SSHPASS} scp ${SSH_OPTS}"
+SSH="${SSHPASS} ssh ${SSH_OPTS}"
+TARGET="$MY_USER@popcon.io"
+
+#echo ${SSH}
+
+function execute() {
+  local msg=$1
+  local cmd=$2
+
+  echo ${msg}
+  ${SSH} ${TARGET} ${cmd}
+
+  if [ $? != 0 ]; then
+    exit $?
+  fi
+}
+
+echo "copy to popcon-server"
+${SCP} target/popcon-server.tar.gz ${TARGET}:~/popcon-server.tar.gz
+execute "1. stop server" 'bash ~/popcon-server/bin/stop-server.sh'
+execute "2. remove older" 'rm -rf ~/popcon-server'
+execute "3. unpack distributions" 'tar xzf ~/popcon-server.tar.gz'
+execute "4. start server" 'bash ~/popcon-server/bin/start-server.sh'
+
+exit 0
